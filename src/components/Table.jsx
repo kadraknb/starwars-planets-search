a51f8a77b01a56548078bf6d4a06ecd6a014c163
kadraknb <kadrak02@hotmail.com>
2022-08-22 07:23:12 -0300

    Requisitos 1~3

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,20 +3,20 @@ import getApiStarWars from '../services/getApiStarWars';
 import Context from '../context/Context';
 
 function Table() {
-  const [search, setSearch] = useState({ filterByName: { name: '' } });
   const { apiSW, setApiSW } = useContext(Context);
   const [renderTr, setRenderTr] = useState([{}]);
+  const [search, setSearch] = useState({ filterByName: { name: '' } });
+  const [filterByNumer, setFilterByNumer] = useState({
+    filterByNumericValues: [
+      {
+        column: 'population',
+        comparison: 'maior que',
+        value: 0,
+      },
+    ],
+  });
 
   useEffect(() => { getApiStarWars(setApiSW); }, [setApiSW]);
-
-  // useEffect(() => { setRenderTr(apiSW.results); }, [apiSW]);
-  // console.log(apiSW);
-
-  // const filterTable = () => {
-  //   const res = apiSW.results.filter((aa) => aa
-  //     .name.includes(search.filterByName.name));
-  //   setRenderTr(res);
-  // };
   useEffect(() => {
     const filterTable = () => {
       const res = apiSW.results.filter((aa) => aa
@@ -27,6 +27,27 @@ function Table() {
     filterTable();
   }, [search, apiSW.results]);
 
+  const change = (e) => {
+    setFilterByNumer({
+      filterByNumericValues: [
+        {
+          ...filterByNumer.filterByNumericValues[0],
+          [e.target.name]: e.target.value,
+        },
+      ],
+    });
+  };
+
+  const submitFilter = () => {
+    const { column, comparison, value } = filterByNumer.filterByNumericValues[0];
+    const res = apiSW.results.filter((aa) => {
+      if (comparison === 'maior que') { return Number(aa[column]) > Number(value); }
+      if (comparison === 'menor que') { return Number(aa[column]) < Number(value); }
+      return Number(aa[column]) === Number(value);
+    });
+    setRenderTr(res);
+  };
+
   return (
     <>
       <input
@@ -35,6 +56,42 @@ function Table() {
         onChange={ (e) => setSearch({ filterByName: { name: e.target.value } }) }
         data-testid="name-filter"
       />
+      <form>
+        <select
+          name="column"
+          onChange={ (e) => change(e) }
+          data-testid="column-filter"
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+        <select
+          name="comparison"
+          onChange={ (e) => change(e) }
+          data-testid="comparison-filter"
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          type="number"
+          name="value"
+          value={ filterByNumer.filterByNumericValues[0].value }
+          onChange={ (e) => change(e) }
+          data-testid="value-filter"
+        />
+        <button
+          type="button"
+          onClick={ () => submitFilter() }
+          data-testid="button-filter"
+        >
+          FILTRAR
+        </button>
+      </form>
       <table>
         <thead>
           <tr>
@@ -54,23 +111,23 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          { renderTr.map((aa, i) => (
+          {renderTr.map((aa, i) => (
             <tr key={ i }>
-              <td>{ aa.name }</td>
-              <td>{ aa.rotation_period }</td>
-              <td>{ aa.orbital_period }</td>
-              <td>{ aa.diameter }</td>
-              <td>{ aa.climate }</td>
-              <td>{ aa.gravity }</td>
-              <td>{ aa.terrain }</td>
-              <td>{ aa.surface_water }</td>
-              <td>{ aa.population }</td>
-              <td>{ aa.films }</td>
-              <td>{ aa.created }</td>
-              <td>{ aa.edited }</td>
-              <td>{ aa.url }</td>
+              <td>{aa.name}</td>
+              <td>{aa.rotation_period}</td>
+              <td>{aa.orbital_period}</td>
+              <td>{aa.diameter}</td>
+              <td>{aa.climate}</td>
+              <td>{aa.gravity}</td>
+              <td>{aa.terrain}</td>
+              <td>{aa.surface_water}</td>
+              <td>{aa.population}</td>
+              <td>{aa.films}</td>
+              <td>{aa.created}</td>
+              <td>{aa.edited}</td>
+              <td>{aa.url}</td>
             </tr>
-          )) }
+          ))}
         </tbody>
       </table>
     </>
--- src/context/provider.js
+++ src/context/provider.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Context from './Context';
 
 function FunProvider({ children }) {
-  const [apiSW, setApiSW] = useState({ results: [{name: ''}] });
+  const [apiSW, setApiSW] = useState({ results: [{ name: '' }] });
 
   return (
     <Context.Provider value={ { apiSW, setApiSW } }>
@@ -10,5 +11,7 @@ function FunProvider({ children }) {
     </Context.Provider>
   );
 }
+
+FunProvider.propTypes = { children: PropTypes.shape().isRequired };
 
 export default FunProvider;
